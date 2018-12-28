c6a1236b7bf82dd6d605786d02d0b25e01dc8aea
Li-Gang <jecsynergy@gmail.com>
2018-12-28 10:59:33 +0900

    redemption

---
+++ App.xaml.cs
@@ -0,0 +1,186 @@
+﻿using Prism.Mvvm;
+using RedemptionReportGenerator.Presentation.Services;
+using RedemptionReportGenerator.Properties;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Deployment.Application;
+using System.Reflection;
+
+namespace RedemptionReportGenerator
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application, IVersionService, IThemeService
+    {
+        private const string DEV_ENV_SUBFIX = "_dev";
+        private const string UAT_ENV_SUBFIX = "_uat";
+
+        public static bool IsUAT
+        {
+            get
+            {
+#if UAT
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+
+        public static bool IsDevelopment
+        {
+            get
+            {
+#if DEBUG
+                return true;
+#else
+                return false;
+#endif
+            }
+        }
+
+        public static bool IsProduction
+        {
+            get
+            {
+                return !IsUAT && !IsDevelopment;
+            }
+        }
+
+        public string Version
+        {
+            get
+            {
+                var versionStr = "";
+
+                if (ApplicationDeployment.IsNetworkDeployed)
+                {
+                    versionStr = ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString();
+                }
+                else
+                {
+                    versionStr = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+                }
+
+                if (IsDevelopment)
+                {
+                    versionStr += DEV_ENV_SUBFIX;
+                }
+                else if (IsUAT)
+                {
+                    versionStr += UAT_ENV_SUBFIX;
+                }
+
+                return versionStr;
+            }
+        }
+
+        public int ThemeIndex
+        {
+            get
+            {
+                return Settings.Default.ThemeIndex;
+            }
+            set
+            {
+                ChangeTheme(value);
+
+                Settings.Default.ThemeIndex = value;
+                Settings.Default.Save();
+            }
+        }
+
+        public int ThemeCount
+        {
+            get
+            {
+                var themes = Resources["Themes"] as ArrayList;
+                return themes.Count;
+            }
+        }
+
+        public string GetConnectionString(string name)
+        {
+            if (IsDevelopment)
+            {
+                name += DEV_ENV_SUBFIX;
+            }
+            else if (IsUAT)
+            {
+                name += UAT_ENV_SUBFIX;
+            }
+
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        private void ChangeTheme(int themeDictionaryIndex)
+        {
+            var themes = Resources["Themes"] as ArrayList;
+            ChangeTheme(new Uri(themes[themeDictionaryIndex] as string, UriKind.Relative));
+        }
+
+        private void ChangeTheme(string themeDictionaryPath)
+        {
+            ChangeTheme(new Uri(themeDictionaryPath, UriKind.Relative));
+        }
+
+        private void ChangeTheme(Uri themeDictionaryUri)
+        {
+            var themeDictionaries = Resources.MergedDictionaries.OfType<ThemeResourceDictionary>().ToList();
+            foreach (var themeDictionary in themeDictionaries)
+            {
+                Resources.MergedDictionaries.Remove(themeDictionary);
+            }
+
+            if (themeDictionaryUri != null)
+            {
+                var newThemeDictionary = new ThemeResourceDictionary();
+                newThemeDictionary.Source = themeDictionaryUri;
+
+                Resources.MergedDictionaries.Insert(4, newThemeDictionary);
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var bootstrapper = new Bootstrapper(this);
+
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver(viewType =>
+            {
+                var viewName = viewType.Name;
+                var viewAssemblyName = viewType.GetType().Assembly.FullName;
+                var viewModelName = string.Format(CultureInfo.InvariantCulture, "RedemptionReportGenerator.Presentation.ViewModels.I{0}ViewModel", viewName);
+
+                return Type.GetType(viewModelName);
+            });
+
+            ChangeTheme(ThemeIndex);
+
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+            Exit += (sender, arg) => bootstrapper.Dispose();
+
+            bootstrapper.Run();
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            throw e.Exception;
+        }
+
+        // Not used directly by any code but necessary to initialize Costura.
+        private static void CosturaInit()
+        {
+            CosturaUtility.Initialize();
+        }
+    }
+}
---
+++ Boostrapper.cs
@@ -0,0 +1,122 @@
+﻿using Prism.Unity;
+using RedemptionReportGenerator.DAL;
+using RedemptionReportGenerator.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using System.Windows;
+using RedemptionReportGenerator.Presentation.ViewModels;
+using RedemptionReportGenerator.Presentation.Views;
+using RedemptionReportGenerator.Presentation.Services;
+using RedemptionReportGenerator.Domain.RedemptionTrade;
+using RedemptionReportGenerator.Domain.Branch;
+using RedemptionReportGenerator.Domain.Customer;
+using RedemptionReportGenerator.Domain.Localization;
+using Prism.Events;
+using AutoMapper;
+using MvvmDialogs;
+using RedemptionReportGenerator.Domain.RedemptionSchedule;
+using RedemptionReportGenerator.Domain.Exporter;
+using RedemptionReportGenerator.Domain.Interop;
+using RedemptionReportGenerator.Domain.Utils;
+
+namespace RedemptionReportGenerator
+{
+    class Bootstrapper : UnityBootstrapper, IDisposable
+    {
+        private App MyApp { get; set; }
+
+        public Bootstrapper(App app)
+        {
+            MyApp = app;
+        }
+
+        private void RegisterDAL()
+        {
+            Container.RegisterType<IUnitOfWork, UnitOfWork>(new PerResolveLifetimeManager());
+
+            Container.RegisterType<UnitOfWork.GetBprTradeDBConnectionString>(
+                new InjectionFactory(c =>
+                    new UnitOfWork.GetBprTradeDBConnectionString(() => MyApp.GetConnectionString("BprTradeDBDbContext"))));
+            Container.RegisterType<UnitOfWork.GetTradeDBConnectionString>(
+                new InjectionFactory(c =>
+                new UnitOfWork.GetTradeDBConnectionString(() => MyApp.GetConnectionString("TradeDBDbContext"))));
+        }
+
+        private void RegisterPresentationLayer()
+        {
+            Container.RegisterType<IMainWindowViewModel, MainWindowViewModel>();
+            Container.RegisterType<ITradeListPageViewModel, TradeListPageViewModel>();
+            Container.RegisterType<IRedemptionScheduleListWindowViewModel, RedemptionScheduleListWindowViewModel>();
+
+            Container.RegisterType<FactoryMethods.CreateTradeListPageVM>(
+                new InjectionFactory(c =>
+                    new FactoryMethods.CreateTradeListPageVM(() => c.Resolve<ITradeListPageViewModel>())));
+            Container.RegisterType<FactoryMethods.CreateRedemptionScheduleListWindowVM>(
+                new InjectionFactory(c =>
+                    new FactoryMethods.CreateRedemptionScheduleListWindowVM(() => c.Resolve<IRedemptionScheduleListWindowViewModel>())));
+
+            Container.RegisterType<ILocalizationService, ApplicationLocalizationService>();
+            Container.RegisterType<IRedemptionTradeVMService, RedemptionTradeVMService>();
+            Container.RegisterInstance<IVersionService>(MyApp);
+            Container.RegisterInstance<IThemeService>(MyApp);
+
+            Container.RegisterInstance<IDialogService>(new DialogService());
+        }
+
+        private void RegisterDomainLayer()
+        {
+            Container.RegisterType<IRedemptionTradeService, RedemptionTradeService>();
+            Container.RegisterType<IRedemptionScheduleService, RedemptionScheduleService>();
+            Container.RegisterType<ICustomerService, CustomerService>();
+            Container.RegisterType<IBranchService, BranchService>();
+
+            Container.RegisterInstance<IExcelService>(new ExcelService());
+
+            Container.RegisterType<IKIDateStringConverter, KIDateStringConverter>();
+            Container.RegisterType<IStringToDecimalConverter, StringToDecimalConverter>();
+        }
+
+        private void RegisterApplicationWiseServices()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfiles(typeof(App));
+            });
+
+            var mapper = config.CreateMapper();
+            Container.RegisterInstance(mapper);
+
+            Container.RegisterType<IUserSettingsService, ApplicationUserSettingsService>();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            RegisterApplicationWiseServices();
+
+            RegisterPresentationLayer();
+            RegisterDAL();
+            RegisterDomainLayer();
+        }
+
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        public void Dispose()
+        {
+            Container.Resolve<IExcelService>().Dispose();
+        }
+    }
+}
